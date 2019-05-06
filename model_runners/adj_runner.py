Fix index calculation for flow iteration

model_runners/adj_runner.py
@@ -7,20 +7,19 @@ from core.dataset import DatasetManager, Series, DataSeries
 from model_runners.model_runner import ModelRunner
 from models.adj_neighborhood_model import AdjModel
 
+# This is hard coded only for now.
+MAX_DEGREE = 3
 
 class AdjRunner(ModelRunner):
 
     def create_placeholders(self, model, num_nodes, embedding_size, **kwargs):
         num_neighborhoods = kwargs['num_neighborhoods']
 
-        # This is hard coded only for now.
-        max_degree = 3
-
         # Placeholder shapes
         b = self.params['batch_size']
         node_shape = [b, num_nodes+1, self.num_node_features]
         demands_shape = [b, num_nodes+1, 1]
-        adj_shape = [b, num_nodes+1, max_degree]
+        adj_shape = [b, num_nodes+1, MAX_DEGREE]
         embedding_shape = [b, num_nodes+1, embedding_size]
         num_nodes_shape = [b, 1]
 
@@ -76,27 +75,28 @@ class AdjRunner(ModelRunner):
             node_features = node_features[index]
             adj = adj[index]
             node_embeddings = node_embeddings[index]
-            neighborhoods = neighborhoods[index]
-            capacities = capacities[index]
             dropout_keep = 1.0
 
         demands = np.array([features_to_demands(n) for n in node_features])
 
-        max_degree = 3
-        n_nodes = 4
+        # This won't work for multiple graphs because we already
+        # pad the matrices. This padding, however, should be removed.
+        num_nodes = np.reshape([mat.shape[0] for mat in adj], [-1, 1])
+
+        n_nodes = np.max(num_nodes)
 
         adj_lists = [neighborhood_adj_lists(mat, 1, False)[0][1] for mat in adj]
-        adj_tensors = np.array([pad_adj_list(adj_lst, max_degree, n_nodes) for adj_lst in adj_lists])
+        adj_tensors = np.array([pad_adj_list(adj_lst, MAX_DEGREE, n_nodes) for adj_lst in adj_lists])
 
         inv_adj_lists = [neighborhood_adj_lists(mat, 1, False, True)[0][1] for mat in adj]
-        inv_adj_tensors = np.array([pad_adj_list(adj_lst, 3, n_nodes) for adj_lst in inv_adj_lists])
+        inv_adj_tensors = np.array([pad_adj_list(adj_lst, MAX_DEGREE, n_nodes) for adj_lst in inv_adj_lists])
 
         # 2D indexing used to extract inflow
         indices = np.zeros(shape=(np.prod(adj_tensors.shape), 3))
         index = 0
         for x in range(adj_tensors.shape[0]):
             for y in range(adj_tensors.shape[1]):
-                for i, z in enumerate(adj_tensors[x, y]):
+                for i, z in enumerate(inv_adj_tensors[x, y]):
                     indexof = np.where(adj_tensors[x, z] == y)[0]
 
                     indices[index, 0] = x
@@ -105,23 +105,18 @@ class AdjRunner(ModelRunner):
                     if len(indexof) > 0:
                         indices[index, 2] = indexof[0]
                     else:
-                        indices[index, 2] = max_degree-1
+                        indices[index, 2] = MAX_DEGREE-1
 
                     index += 1
-        
 
         # Add dummy embeddings, features and demands
         demands = np.insert(demands, demands.shape[1], 0, axis=1)
-        
+
         node_features = np.array(node_features)
         node_features = np.insert(node_features, node_features.shape[1], 0, axis=1)
 
         node_embeddings = np.array(node_embeddings)
         node_embeddings = np.insert(node_embeddings, node_embeddings.shape[1], 0, axis=1)
-
-        # This won't work for multiple graphs because we already
-        # pad the matrices. This padding, however, should be removed.
-        num_nodes = np.reshape([mat.shape[0] for mat in adj], [-1, 1])
 
         feed_dict = {
             placeholders['node_features']: node_features,

models/adj_neighborhood_model.py
@@ -91,16 +91,16 @@ class AdjModel(Model):
                 # Normalize weights for outgoing neighbors
                 normalized_weights = tf.nn.softmax(pred_weights, axis=-1)
 
-                flow = mcf_solver(pred_weights=normalized_weights,
-                                  demand=demands,
-                                  flow_indices=flow_indices,
-                                  max_iters=self.params['flow_iters'])
+                flow, pflow = mcf_solver(pred_weights=normalized_weights,
+                                         demand=demands,
+                                         flow_indices=flow_indices,
+                                         max_iters=self.params['flow_iters'])
 
                 flow_cost = tf.reduce_sum(self.cost_fn.apply(flow), axis=[1, 2])
 
                 self.loss = flow_cost
                 self.loss_op = tf.reduce_mean(self.loss)
-                self.output_ops += [node_weights]
+                self.output_ops += [flow, flow_cost, adj_lst, normalized_weights, pflow]
                 self.optimizer_op = self._build_optimizer_op()
 
                 # # Compute minimum cost flow from flow weights

utils/flow_utils.py
@@ -11,6 +11,7 @@ def mcf_solver(pred_weights, demand, flow_indices, max_iters):
 
     Returns: B x (V+1) x D tensor containing flow volumes
     """
+
     def body(flow, prev_flow):
         # Get incoming flows, B * (V+1) * D x 1  tensor
         inflow = tf.gather_nd(flow, flow_indices)
@@ -33,9 +34,10 @@ def mcf_solver(pred_weights, demand, flow_indices, max_iters):
     flow = tf.zeros_like(pred_weights, dtype=tf.float32)
     prev_flow = flow + BIG_NUMBER
     shape_invariants = [flow.get_shape(), prev_flow.get_shape()]
-    flow, pflow = tf.while_loop(cond, body,
+    flow, pflow = tf.while_loop(cond=cond,
+                                body=body,
                                 loop_vars=[flow, prev_flow],
                                 parallel_iterations=1,
                                 shape_invariants=shape_invariants,
                                 maximum_iterations=max_iters)
-    return flow
+    return flow, pflow