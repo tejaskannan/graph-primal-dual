Fix numerical instability issues in sparsemax and integrate with adj list model

core/layers.py
@@ -354,14 +354,16 @@ class SparseMax(Layer):
         obs_indices = tf.concat([indices_x, indices_y], axis=-1)
 
         # k(z) indices within the final dimension of the 3D tensor
-        indices = tf.concat([obs_indices, tf.reshape(k_z - 1, [-1, 1])], axis=1)
+        k_z_indices = tf.nn.relu(tf.reshape(k_z - 1, [-1, 1]))
+        indices = tf.concat([obs_indices, k_z_indices], axis=1)
 
         # Partial sums less than (z)
         tau_sum = tf.gather_nd(partial_sums, indices)
         tau_sum_reshape = tf.reshape(tau_sum, tf.shape(k_z))
 
         # Threshold value tau(z)
-        tau_z = (tau_sum_reshape - 1) / tf.cast(k_z, dtype=z.dtype)
+        k_z = tf.clip_by_value(tf.cast(k_z, dtype=z.dtype), SMALL_NUMBER, BIG_NUMBER)
+        tau_z = (tau_sum_reshape - 1) / k_z
         tau_z = tf.expand_dims(tau_z, axis=-1)
 
         # Take max of reduced z values with zero

models/adj_neighborhood_model.py
@@ -91,8 +91,16 @@ class AdjModel(Model):
                                                 mask_index=num_nodes)
                 pred_weights = tf.squeeze(pred_weights, axis=-1)
 
+                # Mask to remove nonexistent edges
+                mask_indices = tf.expand_dims(num_nodes, axis=-1)
+                mask = 1.0 - tf.cast(tf.equal(adj_lst, mask_indices), tf.float32)
+
                 # Normalize weights for outgoing neighbors
-                normalized_weights = tf.nn.softmax(pred_weights, axis=-1)
+                if self.params['use_sparsemax']:
+                    sparsemax = SparseMax(epsilon=1e-5)
+                    normalized_weights = sparsemax(inputs=pred_weights, mask=mask)
+                else:
+                    normalized_weights = tf.nn.softmax(pred_weights, axis=-1)
 
                 flow, pflow = mcf_solver(pred_weights=normalized_weights,
                                          demand=demands,
@@ -114,9 +122,6 @@ class AdjModel(Model):
                                    activate_final=False,
                                    name='dual-decoder')
                 dual_vars = dual_decoder(inputs=node_encoding)
-
-                mask_indices = tf.expand_dims(num_nodes, axis=-1)
-                mask = 1.0 - tf.cast(tf.equal(adj_lst, mask_indices), tf.float32)
 
                 # B x (V + 1) x D tensor of repeated dual variables
                 dual = mask * dual_vars