Fix nesterov lookahead in dual flow computation

models/adj_neighborhood_model.py
@@ -135,7 +135,9 @@ class AdjModel(Model):
                                            mask_index=num_nodes,
                                            set_zero=True)
                 dual_tr = tf.squeeze(dual_tr, axis=-1)
-                dual_diff = dual - dual_tr
+
+                # alpha_j - alpha_i
+                dual_diff = dual_tr - dual
 
                 # B x V x D
                 dual_flows = dual_flow(dual_diff=dual_diff,
@@ -146,10 +148,10 @@ class AdjModel(Model):
                                        max_iters=self.params['dual_iters'])
 
                 dual_demand = tf.reduce_sum(dual_vars * demands, axis=[1, 2])
-                dual_flow_cost = self.cost_fn.apply(dual_flows) - dual_diff * dual_flows
+                dual_flow_cost = self.cost_fn.apply(dual_flows) + dual_diff * dual_flows
                 dual_cost = tf.reduce_sum(dual_flow_cost, axis=[1, 2]) - dual_demand
 
                 self.loss = flow_cost - dual_cost
                 self.loss_op = tf.reduce_mean(self.loss)
-                self.output_ops += [flow, flow_cost, adj_lst, normalized_weights, dual_cost, node_weights]
+                self.output_ops += [flow, flow_cost, adj_lst, normalized_weights, dual_cost, node_weights, dual_flows, dual_diff, dual_vars]
                 self.optimizer_op = self._build_optimizer_op()

utils/flow_utils.py
@@ -47,7 +47,8 @@ def dual_flow(dual_diff, adj_mask, cost_fn, step_size, momentum, max_iters, name
 
         def body(flow, acc, prev_flow):
             momentum_acc = momentum * acc
-            derivative = cost_fn.derivative(flow - momentum_acc) - dual_diff
+            predicted = tf.nn.relu(adj_mask * (flow - momentum_acc))
+            derivative = cost_fn.derivative(predicted) + dual_diff
             next_acc = momentum_acc + step_size * derivative
             next_flow = tf.nn.relu(adj_mask * (flow - next_acc))
             return [next_flow, next_acc, flow]